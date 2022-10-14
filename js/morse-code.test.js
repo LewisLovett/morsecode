c28a226944b5b3ad1b2284ec237cc9d512cb8145
Lewis Lovett <llovett7575@gmail.com>
2022-10-14 10:42:52 +0100

    Created test case

---
+++ js/morse-code.test.js
@@ -0,0 +1,46 @@
+const {translateToMorse} = require('./morse-code.js');
+
+describe(translateToMorse,() => {
+    it("B should translate to -...", () => {
+        const result = translateToMorse("B");
+        expect(result).toEqual("-...");
+    });
+
+    it("b should translate to -...", () => {
+        const result = translateToMorse("b");
+        expect(result).toEqual("-...");
+    });
+
+    it("tAb should translate to - .- -...", () => {
+        const result = translateToMorse("tAb");
+        expect(result).toEqual("- .- -...");
+    });
+
+    it("B aT should translate to - .- -...", () => {
+        const result = translateToMorse("B aT");
+        expect(result).toEqual("-... .- -");
+    });
+
+    it("5 should translate to ....", () => {
+        const result = translateToMorse(5);
+        expect(result).toEqual("....");
+    });
+
+    it("& should translate to .-...", () => {
+        const result = translateToMorse("&");
+        expect(result).toEqual(".-...");
+    });
+    it("An empty input returns nothing", () => {
+        const result = translateToMorse("");
+        expect(result).toEqual("");
+    });
+    it("> should translate to invalid", () => {
+        const result = translateToMorse(">");
+        expect(result).toEqual("invalid");
+    });
+    it(">p should translate to invalid", () => {
+        const result = translateToMorse(">p");
+        expect(result).toEqual("invalid .--.");
+    });
+
+});
