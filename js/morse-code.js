59e96ac1f1e5a72f13fd0c36e60bf5f61e7df1f1
Lewis Lovett <llovett7575@gmail.com>
2022-10-14 12:11:45 +0100

    Seperated translateToMorse into new function

--- js/morse-code.js
+++ js/morse-code.js
@@ -1,47 +1,52 @@
 const morseCharArry = [
-    {"charKey":"A","morseC":".-"},
-    {"charKey":"B","morseC":""},
-    {"charKey":"C","morseC":""},
-    {"charKey":"D","morseC":""},
-    {"charKey":"E","morseC":""},
-    {"charKey":"F","morseC":""},
-    {"charKey":"G","morseC":""},
-    {"charKey":"H","morseC":""},
-    {"charKey":"I","morseC":""},
-    {"charKey":"J","morseC":""},
-    {"charKey":"K","morseC":""},
-    {"charKey":"L","morseC":""},
-    {"charKey":"M","morseC":""},
-    {"charKey":"N","morseC":""},
-    {"charKey":"O","morseC":""},
-    {"charKey":"P","morseC":""},
-    {"charKey":"Q","morseC":""},
-    {"charKey":"R","morseC":""},
-    {"charKey":"S","morseC":""},
-    {"charKey":"T","morseC":""},
-    {"charKey":"U","morseC":""},
-    {"charKey":"V","morseC":""},
-    {"charKey":"W","morseC":""},
-    {"charKey":"X","morseC":""},
-    {"charKey":"Y","morseC":""},
-    {"charKey":"Z","morseC":""},
+    {"charKey":"A","morseCode":".-"},
+    {"charKey":"B","morseCode":"-..."},
+    {"charKey":"C","morseCode":"-.-."},
+    {"charKey":"D","morseCode":"-.."},
+    {"charKey":"E","morseCode":"."},
+    {"charKey":"F","morseCode":"..-."},
+    {"charKey":"G","morseCode":"--."},
+    {"charKey":"H","morseCode":"...."},
+    {"charKey":"I","morseCode":".."},
+    {"charKey":"J","morseCode":".---"},
+    {"charKey":"K","morseCode":"-.-"},
+    {"charKey":"L","morseCode":".-.."},
+    {"charKey":"M","morseCode":"--"},
+    {"charKey":"N","morseCode":"-."},
+    {"charKey":"O","morseCode":"---"},
+    {"charKey":"P","morseCode":".--."},
+    {"charKey":"Q","morseCode":"--.-"},
+    {"charKey":"R","morseCode":".-."},
+    {"charKey":"S","morseCode":"..."},
+    {"charKey":"T","morseCode":"-"},
+    {"charKey":"U","morseCode":"..-"},
+    {"charKey":"V","morseCode":"...-"},
+    {"charKey":"W","morseCode":".--"},
+    {"charKey":"X","morseCode":"-..-"},
+    {"charKey":"Y","morseCode":"-.--"},
+    {"charKey":"Z","morseCode":"--.."}
 ]
 
-const filterValidChars = (wordInput) => {
+export const filterValidChars = (wordInput) => {
     const wordArry = wordInput.split("");
     const regFilter = new RegExp( `([A-Z0-9&'@)(:,=!.%+"?/])`, `gi`)
     const filteredWordArray = wordArry.filter((arryChar) => arryChar.match(regFilter));
     return filteredWordArray;
 }
 
-const translateToMorse = (charsArray) => {
+export const translateArray = (charsArray) => {
     let morseString = "";
     charsArray.forEach(wordChar => {
         wordChar = wordChar.toUpperCase();
-        let obj =morseCharArry.find(o => o.charKey == wordChar);
-        morseString += obj.morseC;
+        let morseObj = morseCharArry.find(o => o.charKey == wordChar);
+        morseString += morseObj.morseCode;
+        morseString += " "
+
     });
     return morseString;
 }
 
-console.log(translateToMorse(["A"]));
+export const translateToMorse = (wordInput) => {
+    const filteredWordArray = filterValidChars(wordInput);
+    return translateArray(filteredWordArray);
+}
