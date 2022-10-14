47e39d7780560e065ac054f3949c2526763b19e1
Lewis Lovett <llovett7575@gmail.com>
2022-10-14 11:56:22 +0100

    Created array for char translations

--- js/morse-code.js
+++ js/morse-code.js
@@ -1,3 +1,32 @@
+const morseCharArry = [
+    {"charKey":"A","morseC":".-"},
+    {"charKey":"B","morseC":""},
+    {"charKey":"C","morseC":""},
+    {"charKey":"D","morseC":""},
+    {"charKey":"E","morseC":""},
+    {"charKey":"F","morseC":""},
+    {"charKey":"G","morseC":""},
+    {"charKey":"H","morseC":""},
+    {"charKey":"I","morseC":""},
+    {"charKey":"J","morseC":""},
+    {"charKey":"K","morseC":""},
+    {"charKey":"L","morseC":""},
+    {"charKey":"M","morseC":""},
+    {"charKey":"N","morseC":""},
+    {"charKey":"O","morseC":""},
+    {"charKey":"P","morseC":""},
+    {"charKey":"Q","morseC":""},
+    {"charKey":"R","morseC":""},
+    {"charKey":"S","morseC":""},
+    {"charKey":"T","morseC":""},
+    {"charKey":"U","morseC":""},
+    {"charKey":"V","morseC":""},
+    {"charKey":"W","morseC":""},
+    {"charKey":"X","morseC":""},
+    {"charKey":"Y","morseC":""},
+    {"charKey":"Z","morseC":""},
+]
+
 const filterValidChars = (wordInput) => {
     const wordArry = wordInput.split("");
     const regFilter = new RegExp( `([A-Z0-9&'@)(:,=!.%+"?/])`, `gi`)
@@ -9,13 +38,10 @@ const translateToMorse = (charsArray) => {
     let morseString = "";
     charsArray.forEach(wordChar => {
         wordChar = wordChar.toUpperCase();
-        switch(wordChar){
-            case "A":
-                morseString += ".-";
-                break;
-        }
+        let obj =morseCharArry.find(o => o.charKey == wordChar);
+        morseString += obj.morseC;
     });
     return morseString;
 }
 
-console.log(translateToMorse(["A","a","&"]));
+console.log(translateToMorse(["A"]));
